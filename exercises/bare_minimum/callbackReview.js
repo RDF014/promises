c9f441f4281194e9afa548d93ae144c3b76e2abf
Tiffany <tiffanylin27@gmail.com>
2017-03-02 19:21:21 -0800

    Pass first test

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -3,16 +3,37 @@
  */
 
 var fs = require('fs');
+var readline = require('readline');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+
+  fs.readFile(filePath, function(err, data) {
+    console.log('DATA: ', data);
+    console.log('TYPEOF DATA: ', typeof data);
+    var rl = readline.createInterface({
+      input: fs.createReadStream(filePath)
+    });
+    rl.on('line', function(line) {
+      console.log('HERE IS THE LINE: ', line);
+      callback(err, line);
+    });
+    callback(err, data);
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCode = function (url) {
   // TODO
+  var code = undefined;
+  request(url, function (err, response) {
+    if (!err) {
+      code = response.statusCode;
+    }
+  });
+  return code;
 };
 
 // Export these functions so we can test them and reuse them in later exercises
